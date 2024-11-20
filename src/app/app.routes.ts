a52386f2d1d616ba6daf54bc2f7ffd1a3b3ac73a
pao407 <paoberu407@gmail.com>
2024-11-19 19:14:45 -0500

    avanzo en la creacion de la vista de crear

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './components/home/home.component';
+import { CrearComponent } from './components/crear/crear.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: CrearComponent}
     //{ path: 'login', component: LoginComponent },
 ];
